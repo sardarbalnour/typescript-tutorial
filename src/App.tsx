93905dccc04972827d4861ac77c679df07423451
Sardar balnour <balnour.sardar@gmail.com>
2026-09-10 11:45:12 +0330

    declare type

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+type User2 = { name: string; age: number };
+type ArrMix = (string | number)[];
+
 function App() {
   let name: string = "TypeScript";
   console.log(name);
@@ -16,11 +19,13 @@ function App() {
   const arr2: number[] = [2, 3, 4];
   const arr3: string[] = ["x"];
 
-  const arrMix: (string | number)[] = [1, "s"];
+  // const arrMix: (string | number)[] = [1, "s"];
+  const arrMix: ArrMix = [1, "s"];
 
   const obj = { name: "ali", age: 22, color: "white" };
 
-  const obj2: { name: string; age: number } = { name: "sardar", age: 26 };
+  // const obj2: { name: string; age: number } = { name: "sardar", age: 26 };
+  const obj2: User2 = { name: "sardar", age: 26 };
 
   return (
     <>
