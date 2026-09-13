0d624833e926dd91040f2f0894a0d9ac2ae6e289
Sardar balnour <balnour.sardar@gmail.com>
2026-09-13 11:23:37 +0330

    union types

--- src/components/Function.tsx
+++ src/components/Function.tsx
@@ -8,9 +8,16 @@ function Function() {
   return (
     <div>
       <h2>{sum(5, 3)}</h2>
-      <Prop name="sardar" age={26} hasPet={true} sum={sum} />
+      <Prop name="sardar" age={26} hasPet={true} sum={sum} mother="bahar" />
       <hr />
-      <Prop name="sardar" age={26} hasPet={true} sum={sum} father="John" />
+      <Prop
+        name="sardar"
+        age={26}
+        hasPet={true}
+        sum={sum}
+        father="John"
+        mother="maryam"
+      />
     </div>
   );
 }
--- src/components/Prop.tsx
+++ src/components/Prop.tsx
@@ -1,18 +1,22 @@
+type motherType = "bahar" | "maryam" | "fatemeh";
+
 type PropData = {
   name: string;
   age: number;
   hasPet: boolean;
   father?: string;
+  mother: motherType;
   sum: (a: number, b: number) => number;
 };
 
-function Prop({ name, age, hasPet, father, sum }: PropData) {
+function Prop({ name, age, hasPet, father, mother, sum }: PropData) {
   return (
     <div>
       <p>Name: {name}</p>
       <p>Age: {age}</p>
       <p>Has Pet: {hasPet.toString()}</p>
       <p>Father: {father || "-"}</p>
+      <p>Mother: {mother}</p>
       <p>Sum: {sum(15, 3)}</p>
     </div>
   );
