651e41057cb3ae3185a69d6583d663868f7ea350
Sardar balnour <balnour.sardar@gmail.com>
2026-09-20 17:35:59 +0330

    ComponentPropsWithRef

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,12 @@
 import type { ComponentProps } from "react";
+// ComponentProps is a utility type that extracts the props of a given component type.
+//  In this case, it is used to extract the props of the "button" HTML element.
+//  The Button component extends these props and adds an optional buttonTitle prop,
+//  which allows for customizing the button's text. If buttonTitle is not provided, it defaults to "click".
+
+// ComponentPropsWithRef is a utility type that extracts the props of a given component type, including the ref prop.
+
+// ComponentPropsWithoutRef is a utility type that extracts the props of a given component type, excluding the ref prop.
 
 type Button = ComponentProps<"button"> & {
   buttonTitle?: string;
